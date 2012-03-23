99508b471a81fa633928909c0d0693bcce8f9559
Matthew Monroe <matthew.monroe@pnnl.gov>
2012-03-23 23:58:41 +0000

    Initial commit

---
+++ MSDataFileReader.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Original author: Matthew Monroe <matthew.monroe .at. pnnl.gov>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using pwiz.CLI.cv;
+using pwiz.CLI.data;
+using pwiz.CLI.msdata;
+
+namespace pwiz.ProteowizardWrapper
+{
+	class MSDataFileReader : MsDataFileImpl
+	{
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="msDataFile">MSData object</param>
+		public MSDataFileReader(MSData msDataFile) : base(msDataFile)
+        {
+			// Call the base class constructor
+			;
+        }
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="path">Data file path</param>
+		public MSDataFileReader(string path) : base(path)
+        {
+			// Call the base class constructor
+			;
+        }
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="path">Data file path</param>
+		/// <param name="sampleIndex">Sample Index to select within the data file</param>
+		public MSDataFileReader(string path, int sampleIndex) : base (path, sampleIndex)
+        {
+			// Call the base class constructor
+			;       
+        }
+
+		public CVParamList GetChromatogramCVParams(int chromIndex)
+		{
+			return ChromatogramList.chromatogram(chromIndex).cvParams;
+		}
+
+		public Chromatogram GetChromatogramObject(int chromIndex)
+		{
+			return ChromatogramList.chromatogram(chromIndex, true);
+		}
+
+		public CVParamList GetSpectrumCVParams(int scanIndex)
+		{
+			return SpectrumList.spectrum(scanIndex).cvParams;
+		}
+
+		public Spectrum GetSpectrumObject(int scanIndex)
+		{
+			return SpectrumList.spectrum(scanIndex, true);
+		}
+
+
+	}
+}
---
+++ Readme.txt
@@ -0,0 +1,31 @@
+The ProteowizardWrapper is a series of C# class that can be used to interface with the proteowizard libraries.
+
+This code was written by Nick Shulman for the MacCoss Lab at the University of Washington in 2009
+
+Matthew Monroe added class MSDataFileReader in 2012 to extend class MsDataFileImpl with additional functions
+for directly accessing the pwiz data objects.
+
+
+The original source code to the ProteowizardWrapper is distributed as part of the ProteowizardWrapper library.
+It is located at:
+  pwiz_tools\Shared\ProteowizardWrapper
+
+
+ProteowizardWrapper web pages:
+http://proteowizard.sourceforge.net/
+http://sourceforge.net/projects/proteowizard
+
+Can download a Windows installer from:
+http://proteowizard.sourceforge.net/downloads.shtml
+
+Alternatively, to download a .tar.bz2 file with the latest .Exe files:
+1) Go to http://teamcity.labkey.org:8080/project.html?projectId=project4 and click "Login as Guest User"
+2) Find "Windows x86" in the list
+3) If necessary, expand it by clicking the Plus sign
+4) Just to the right of the word "Artifacts" is a down arrow; click it to reveal a dropdown list
+5) Choose the latest *-bin-windows-x86*.tar.bz2 file
+6) After downloading the file, extract the data using 7-zip (http://www.7-zip.org/) or WinRar
+
+To download a .tar.gz file with the source code:
+1) Go to http://proteowizard.svn.sourceforge.net/viewvc/proteowizard/trunk/pwiz/
+2) Click "Download GNU tarball"
