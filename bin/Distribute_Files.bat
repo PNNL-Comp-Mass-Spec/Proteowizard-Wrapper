436e77e8abed8c46558532356289872acf592af1
alchemistmatt <matthew.monroe@pnnl.gov>
2019-04-11 13:04:44 -0700

    Update batch file

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -2,27 +2,27 @@
 echo Copying the DLL
 @echo on
 
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\Lib\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\Release\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\Release\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\Debug\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\Lib\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\Release\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\Release\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\Debug\" /D /Y
 
-xcopy Release\ProteowizardWrapper.pdb "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\Lib\" /D /Y
-xcopy Release\ProteowizardWrapper.pdb "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\" /D /Y
-xcopy Release\ProteowizardWrapper.pdb "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\" /D /Y
-xcopy Release\ProteowizardWrapper.pdb "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\Release\" /D /Y
-xcopy Release\ProteowizardWrapper.pdb "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\Release\" /D /Y
-xcopy Release\ProteowizardWrapper.pdb "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\Debug\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\Lib\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\Release\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\Release\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\Debug\" /D /Y
 
 
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Debug\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Release\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Debug\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Release\" /D /Y
 
-xcopy Release\ProteowizardWrapper.dll "F:\My Documents\Projects\Instrument-Software\LCMS-Spectator\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\Instrument-Software\LCMS-Spectator\Library\" /D /Y
 
 @echo off
 pause
