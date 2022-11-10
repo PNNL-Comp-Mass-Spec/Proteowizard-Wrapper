7ffc76838e8e631b107505368427050dd086365c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-10 15:53:16 -0800

    Copy additional files to the DeconTools library directory

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -20,6 +20,9 @@ xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_
 xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\Debug\" /D /Y
 
 xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.xml "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
+
 xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\" /D /Y
 xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Debug\" /D /Y
 xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Release\" /D /Y
