9843c09748bf3f1fc029dd3ce827cff2091cd7de
alchemistmatt <matthew.monroe@pnnl.gov>
2025-10-22 18:27:01 -0700

    Update batch distribution file

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -19,19 +19,19 @@ xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_
 xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\Release\" /D /Y
 xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\UnitTests\bin\Debug\" /D /Y
 
-xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
-xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
-xcopy Release\ProteowizardWrapper.xml "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\Gordon_Slysz\DeconTools_IQ\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.pdb "F:\Documents\Projects\Gordon_Slysz\DeconTools_IQ\Library\" /D /Y
+xcopy Release\ProteowizardWrapper.xml "F:\Documents\Projects\Gordon_Slysz\DeconTools_IQ\Library\" /D /Y
 
-xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Debug\" /D /Y
-xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\GordonSlysz\DeconTools_IQ\DeconConsole\bin\x86\Release\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\Gordon_Slysz\DeconTools_IQ\DeconConsole\bin\x64\Debug\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\Gordon_Slysz\DeconTools_IQ\DeconConsole\bin\x86\Debug\" /D /Y
+xcopy Release\ProteowizardWrapper.dll "F:\Documents\Projects\Gordon_Slysz\DeconTools_IQ\DeconConsole\bin\x86\Release\" /D /Y
 
 xcopy Release\ProteowizardWrapper.dll "C:\DMS_Programs\MSFileInfoScanner" /D /Y
 
 @echo off
 echo.
 echo You must manually copy ProteowizardWrapper.dll to
-echo \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MSFileInfoScanner
+echo \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\MSFileInfoScanner
 echo.
 pause
